0720b9f95114c154f3aa5ed284e72bf99753be66
Ravi Bhushan <jicsoninfo@gmail.com>
2025-07-09 18:43:11 +0530

    added navbar

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -8,21 +8,74 @@
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<App />);
 
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import LoginPage from './pages/LoginPage';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../css/app.css';
+
+
+
+// const root = document.getElementById('root');
+
+// if (root) {
+//     ReactDOM.createRoot(root).render(
+//         <React.StrictMode>
+//             <LoginPage />
+//         </React.StrictMode>
+//     );
+// }
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LoginPage from './pages/LoginPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../css/app.css';
+import Navbar from './components/Navbar';
 
-
-
-const root = document.getElementById('root');
-
-if (root) {
-    ReactDOM.createRoot(root).render(
-        <React.StrictMode>
-            <LoginPage />
-        </React.StrictMode>
+function App() {
+    return (
+        <div>
+            <Navbar />
+        </div>
     );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+// // resources/js/app.jsx
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import Navbar from './components/Navbar';
+// import LoginPage from './pages/LoginPage';
+// import HomePage from './pages/HomePage';
+// import BlogsPage from './pages/BlogsPage';
+// import RegisterPage from './pages/RegisterPage';
+
+// function App() {
+//     return (
+//         <BrowserRouter>
+//             <Navbar />
+//             <Routes>
+//                 <Route path="/" element={<HomePage />} />
+//                 <Route path="/blogs" element={<BlogsPage />} />
+//                 <Route path="/login" element={<LoginPage />} />
+//                 <Route path="/register" element={<RegisterPage />} />
+//             </Routes>
+//         </BrowserRouter>
+//     );
+// }
+
+// const root = document.getElementById('root');
+
+// if (root) {
+//     ReactDOM.createRoot(root).render(
+//         <React.StrictMode>
+//             <App />
+//         </React.StrictMode>
+//     );
+// }
 
